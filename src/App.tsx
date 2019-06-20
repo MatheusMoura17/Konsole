90d5419454b36e8bb5e2b471446db4302ce07b35
Matheus Moura <matheus.moura13@gmail.com>
2019-06-20 01:01:43 -0300

    Adiciona roteador de páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Console from "./pages/console";
+import Home from "./pages/Home";
+import Console from "./pages/Console";
+import Controller from "./pages/Controller";
 
 /** Aplição principal */
 const App = () => {
-  return <Console />;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/console/" component={Console} />
+      <Route path="/controller/" component={Controller} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Controller.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+const Controller = () => {
+  return <div>controller</div>;
+};
+export default Controller;
